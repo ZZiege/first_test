103fb377be97a317a4b827307a943afae17d2a92
ZZiege <samuli.ikola@gmail.com>
2016-09-06 13:26:54 +0300

    Delete detect_vbox.py

--- detect_vbox.py
+++
@@ -1,8 +0,0 @@
-#!/usr/binpython
-
-vbox_mac = "08:00:27"
-mac_file = '/sys/class/net/eth0/address'
-infile = open(mac_file, "r")
-line = infile.readline()
-if line.startswith(vbox_mac):
-    print 'Running inside vbox' 
